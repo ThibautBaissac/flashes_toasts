eea64620ec56c1192ad6f47f8695988e8f4ca65b
thibautbaissac <tbaissac@gmail.com>
2021-12-22 14:31:53 +0100

    Implement logic

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,23 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.order(created_at: :desc)
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.turbo_stream
+        format.html { redirect_to @tweet, notice: "Tweet was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
